3a81af3b5300529427df438d6ced5263e5bbfe85
thuan-utc <thuansl99@gmail.com>
2023-11-21 06:55:32 +0700

    refract code

--- polls-app-server/src/main/java/utc/cntt2/k61/pollsappserver/controller/AuthController.java
+++ polls-app-server/src/main/java/utc/cntt2/k61/pollsappserver/controller/AuthController.java
@@ -9,27 +9,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import utc.cntt2.k61.pollsappserver.domain.Role;
-import utc.cntt2.k61.pollsappserver.domain.RoleName;
 import utc.cntt2.k61.pollsappserver.domain.User;
 import utc.cntt2.k61.pollsappserver.dto.ApiResponse;
 import utc.cntt2.k61.pollsappserver.dto.JwtAuthenticationResponse;
 import utc.cntt2.k61.pollsappserver.dto.LoginRequest;
 import utc.cntt2.k61.pollsappserver.dto.SignUpRequest;
-import utc.cntt2.k61.pollsappserver.exception.AppException;
-import utc.cntt2.k61.pollsappserver.repository.RoleRepository;
-import utc.cntt2.k61.pollsappserver.repository.UserRepository;
 import utc.cntt2.k61.pollsappserver.security.JwtTokenProvider;
+import utc.cntt2.k61.pollsappserver.service.UserService;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -38,22 +32,17 @@ public class AuthController {
     private final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider tokenProvider;
+
+    private final UserService userService;
 
     @Autowired
     public AuthController(AuthenticationManager authenticationManager,
-                          UserRepository userRepository,
-                          RoleRepository roleRepository,
-                          PasswordEncoder passwordEncoder,
-                          JwtTokenProvider tokenProvider) {
+                          JwtTokenProvider tokenProvider,
+                          UserService userService) {
         this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
         this.tokenProvider = tokenProvider;
+        this.userService = userService;
     }
 
 
@@ -75,28 +64,18 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
+        if(userService.hasExistingUserByUserName(signUpRequest)) {
             return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
                     HttpStatus.BAD_REQUEST);
         }
 
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
+        if(userService.hasExistingUserByEmail(signUpRequest)) {
             return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
                     HttpStatus.BAD_REQUEST);
         }
 
         // Creating user's account
-        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
-                signUpRequest.getEmail(), signUpRequest.getPassword());
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
-                .orElseThrow(() -> new AppException("User Role not set."));
-
-        user.setRoles(Collections.singleton(userRole));
-
-        User result = userRepository.save(user);
+        User result = userService.createNewUser(signUpRequest);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentContextPath().path("/users/{username}")
@@ -104,4 +83,6 @@ public class AuthController {
 
         return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
     }
+
+
 }
--- polls-app-server/src/main/java/utc/cntt2/k61/pollsappserver/service/UserService.java
+++ polls-app-server/src/main/java/utc/cntt2/k61/pollsappserver/service/UserService.java
@@ -3,13 +3,21 @@ package utc.cntt2.k61.pollsappserver.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import utc.cntt2.k61.pollsappserver.domain.Role;
+import utc.cntt2.k61.pollsappserver.domain.RoleName;
 import utc.cntt2.k61.pollsappserver.domain.User;
+import utc.cntt2.k61.pollsappserver.dto.SignUpRequest;
 import utc.cntt2.k61.pollsappserver.dto.UserProfile;
+import utc.cntt2.k61.pollsappserver.exception.AppException;
 import utc.cntt2.k61.pollsappserver.exception.ResourceNotFoundException;
 import utc.cntt2.k61.pollsappserver.repository.PollRepository;
+import utc.cntt2.k61.pollsappserver.repository.RoleRepository;
 import utc.cntt2.k61.pollsappserver.repository.UserRepository;
 import utc.cntt2.k61.pollsappserver.repository.VoteRepository;
+
+import java.util.Collections;
 
 @Service
 public class UserService {
@@ -18,14 +26,20 @@ public class UserService {
     private final UserRepository userRepository;
     private final PollRepository pollRepository;
     private final VoteRepository voteRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository,
                        PollRepository pollRepository,
-                       VoteRepository voteRepository) {
+                       VoteRepository voteRepository,
+                       RoleRepository roleRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.pollRepository = pollRepository;
         this.voteRepository = voteRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public boolean isAvailableEmail(String email) {
@@ -46,5 +60,28 @@ public class UserService {
         UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt(), pollCount, voteCount);
 
         return userProfile;
+    }
+
+    public User createNewUser(SignUpRequest signUpRequest) {
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.save(user);
+        return result;
+    }
+
+    public boolean hasExistingUserByUserName(SignUpRequest signUpRequest) {
+        return userRepository.existsByUsername(signUpRequest.getUsername());
+    }
+
+    public boolean hasExistingUserByEmail(SignUpRequest signUpRequest) {
+        return userRepository.existsByEmail(signUpRequest.getEmail());
     }
 }
